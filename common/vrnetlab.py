8e3fa6a3ad20b3db8ef4535b81547d242554917e
jmac <jocrismachado@gmail.com>
2024-12-15 12:58:19 +0000

    option to reset specific VMs

--- common/vrnetlab.py
+++ common/vrnetlab.py
@@ -696,14 +696,20 @@ class VR:
                 else:
                     self.update_health(1, "starting")
 
-            #backdoor to trigger a system reset on all VMs via qemu-monitor
+            #file-based signalling backdoor to trigger a system reset (via qemu-monitor) on all or specific VMs.
+            #if file is empty: reset whole VR (all VMs)
+            #if file is non-empty: reset only specified VMs (comma separated list)
             if os.path.exists('/reset'):
+                with open('/reset','rt') as f:
+                    fcontent=f.read().strip()
+                vm_num_list=fcontent.split(',')
                 for vm in self.vms:
-                    try:
-                        vm.qm.write("system_reset\r".encode())
-                        self.logger.debug(f"Sent qemu-monitor system_reset to VM num {vm.num} ")
-                    except Exception as e:
-                        self.logger.debug(f"Failed to send qemu-monitor system_reset to VM num {vm.num} ({e})")
+                    if (str(vm.num) in vm_num_list) or not fcontent:
+                        try:
+                            vm.qm.write("system_reset\r".encode())
+                            self.logger.debug(f"Sent qemu-monitor system_reset to VM num {vm.num} ")
+                        except Exception as e:
+                            self.logger.debug(f"Failed to send qemu-monitor system_reset to VM num {vm.num} ({e})")
                 try:
                     os.remove('/reset')
                 except Exception as e:
